a8c2de2d590652f608a8dfd35e9abb4cf3558dd8
han3sui <han3sui@users.noreply.github.com>
2023-11-27 08:06:29 +0000

    deploy: 1195c07f8ea264987ceb4d7c1eec6d767e5899b3

---
+++ assets/chunks/use-merge-state.cdf3b16c.js
@@ -0,0 +1,1 @@
+import{Q as s}from"./index-0c2c2d54.587a412f.js";import{j as n,a4 as r,w as c,m as v}from"./framework.f10db147.js";function m(u){const a=n(u);return[a,t=>{a.value=t}]}function d(u,a){const{value:e}=r(a),[t,l]=m(s(e.value)?u:e.value);return c(e,o=>{s(o)&&l(void 0)}),[v(()=>s(e.value)?t.value:e.value),l,t]}export{d as a,m as u};
