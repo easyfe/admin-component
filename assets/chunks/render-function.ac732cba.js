df0f3057279334adedb4b332d7998703fc50443a
han3sui <han3sui@users.noreply.github.com>
2024-11-12 00:49:58 +0000

    deploy: 61874325e5afedf3d4c388a9fbd3afdd985c0189

--- assets/chunks/render-function.ac732cba.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./framework.6f649eb3.js";var n=e({name:"RenderFunction",props:{renderFunc:{type:Function,required:!0}},render(){return this.renderFunc(this.$attrs)}});export{n as R};
